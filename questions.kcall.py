2a9a04769be22349f13161448d21a08003c7fac4
kcallie4 <111547227+kcallie4@users.noreply.github.com>
2022-12-05 18:27:26 -0500

    Add files via upload

---
+++ questions.kcall.py
@@ -0,0 +1,112 @@
+[
+ {
+            "question": "Who wrote The Odyssey?",
+            "a": "Socrates",
+            "b": "Plato",
+            "c": "Homer",
+            "d": "Shakespeare",
+            "correct answer": "c",
+            "subject": "Literature"
+    },
+
+    {
+            "question": "What is the first item in the periodic table of elements?",
+            "a": "Hydrogen",
+            "b": "Sodium",
+            "c": "Helium",
+            "d": "Aluminum",
+            "correct answer" : "a",
+            "subject": "Science"
+    },
+
+    {
+            "question": "At the Second Continental Congress, who was elected General of the Continental Army?",
+            "a": "John Adams",
+            "b": "Benjamin Franklin",
+            "c": "Samuel Davis",
+            "d": "George Washington",
+            "correct answer": "d",
+            "subject": "History"
+    },
+
+    {
+            "question": "Who was the ruler of Germany during the first World War?",
+            "a": "Helmuth von Moltke",
+            "b": "Friedrich Wilhelm",
+            "c": "Manfred Richtoffen",
+            "d": "Erich Ludendorff",
+            "correct answer": "b",
+            "subject": "History"
+
+    },
+
+    {
+            "question": "In which group of people were the first ones to celebrate Thanksgiving?",
+            "a": "Free Masons",
+            "b": "Quakers",
+            "c": "Pilgrims",
+            "d": "Native Americans",
+            "correct answer": "c",
+            "subject": "History"
+    },
+
+    {
+            "question": "What ancient civilization built the Machu Pichu in Peru?",
+            "a": "The Aztecs",
+            "b": "The Mayans",
+            "c": "The Incas",
+            "d": "The Egyptians",
+            "correct answer": "c",
+            "subject": "Geography"
+    },
+
+    {
+            "question": "In which year did Thomas Edison invent the light bulb?",
+            "a": "1879",
+            "b": "1775",
+            "c": "1895",
+            "d": "1913",
+            "correct answer": "a",
+            "subject": "History"
+    },
+
+    {
+            "question": "Name a famous emperor of the Roman Empire?",
+            "a": "Spartacus",
+            "b": "Otto Octavius",
+            "c": "Julius Caesar",
+            "d": "Pontius Pilate",
+            "correct answer": "c",
+            "subject": "History"
+    },
+
+    {
+            "question": "The first day of the 20th century was...",
+            "a": "January 1st, 1801",
+            "b": "January 1st, 1901",
+            "c": "January 1st, 2001",
+            "d": "January 1st, 2011",
+            "correct answer": "b",
+            "subject": "History"
+    },
+    {
+            "question": "Is Australia considered an island?",
+            "a": "Yes, because it is surrounded by water",
+            "b": "Yes, because it is surrounded by three oceans",
+            "c": "No, a continent cannot be considered an island",
+            "d": "No, it has too many diverse biomes",
+            "correct answer": "c",
+            "subject": "Geography"
+    },
+    {
+            "question": "Which one of these Hololives are not part of Gen 3?",
+            "a": "Houshou Marine",
+            "b": "Shiranui Flare",
+            "c": "Shirakami Fubuki",
+            "d": "Shirogane Noel",
+            "correct answer": "c",
+            "subject": "Hololive"
+    }
+ ]
+ 
+ 
